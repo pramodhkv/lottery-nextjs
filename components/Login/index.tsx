040cd06abf87414f8d878e74da9cebe75db51018
Pramodh Viswanath <pramodh@pink.gg>
2023-02-05 14:58:20 +0530

    Style changes in Login page, removed the background image

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -5,16 +5,20 @@ import CustomButton from "../CustomButton";
 const Login = () => {
   const connectWithMetaMask = useMetamask();
   return (
-    <div className="bg-lotteryLogin bg-no-repeat bg-cover bg-center min-h-screen flex flex-col items-center justify-center text-center text-white p-3">
-      <div className="flex flex-col justify-center items-center mb-10 rounded-lg w-full md:w-1/3 h-96 bg-emerald-900/60">
-        <h1 className="text-5xl font-bold mt-5">Lucky Draw</h1>
+    <div className="bg-body min-h-screen flex flex-col items-center justify-center text-center text-white p-3">
+      <div className="border-2 border-emerald-800 shadow-xl rounded-lg w-full md:w-1/2 container mx-auto p-2 h-96 flex flex-col justify-center">
+        <h1 className="text-5xl md:text-6xl font-bold mt-5 bg-clip-text bg-gradient-to-r from-orange-500 to-emerald-500 text-transparent">
+          Lucky Draw
+        </h1>
         <h2 className="text-lg my-5">Get Started by using Metamask</h2>
 
-        <CustomButton
-          isActive
-          text="Connect with Metamask"
-          onClick={connectWithMetaMask}
-        />
+        <div className="container mx-auto">
+          <CustomButton
+            isActive
+            text="Connect with Metamask"
+            onClick={connectWithMetaMask}
+          />
+        </div>
       </div>
     </div>
   );
